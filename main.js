e3178f97be0219623eb21cf4766fcbee0c3f7a3b
Samuel Pokam <samuel.pokam@cgi.com>
2024-11-11 10:57:53 -0500

    up first try

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
 import "@babel/polyfill"
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import React from "react"
+import ReactDOM from "react-dom"
+import { applyMiddleware, createStore } from "redux"
 
-import Counter from './Counter'
-import reducer from './reducers'
+import Counter from "./Counter"
+import reducer from "./reducers"
 
-const store = createStore(reducer)
+import createSagaMiddleware from "redux-saga"
+import rootSaga, { helloSaga } from "./sagas"
 
-const action = type => store.dispatch({type})
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(reducer, applyMiddleware(sagaMiddleware))
+
+sagaMiddleware.run(rootSaga)
+// const store = createStore(reducer)
+
+const action = (type) => store.dispatch({ type })
 
 function render() {
   ReactDOM.render(
     <Counter
       value={store.getState()}
-      onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
-    document.getElementById('root')
+      onIncrement={() => action("INCREMENT")}
+      onDecrement={() => action("DECREMENT")}
+      onIncrementAsync={() => action("INCREMENT_ASYNC")}
+    />,
+    document.getElementById("root")
   )
 }
 
